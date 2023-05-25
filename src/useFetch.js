d21f2838a74aa7a84efab4acc4c8bf861b0283ae
Sneh Modi <modisneh20@gmail.com>
2023-05-25 12:47:49 +0530

    React Routes

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,22 +4,29 @@ const useFetch = (url) => {
   const [isLoading, setisLoading] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           return res.json();
         })
         .then((result) => {
-          console.log("Data: ", result);
           setData(result);
           setisLoading(false);
           setError(null);
         })
         .catch((err) => {
-          setError(err.message);
-          setisLoading(false);
+          if (err.name === "AbortError") {
+            console.log("Fetched Abort.");
+          } else {
+            setError(err.message);
+            setisLoading(false);
+          }
         });
     }, 1000);
+
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isLoading, error };
